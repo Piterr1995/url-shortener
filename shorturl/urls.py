9060299568823238519dbd514d716c086da1309e
Piotr <mojprogress@gmail.com>
2020-07-13 22:39:18 +0200

    added static url

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("url-generator", views.url_generator, name="url-generator"),
     path("<str:shortened_url>/", views.link_redirect, name="link-redirect"),
-]
-
-urlpatterns += patterns(
-    "",
-    (
-        r"^static/(?P<path>.*)$",
-        "django.views.static.serve",
-        {"document_root": settings.STATIC_ROOT},
-    ),
-)
-
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
