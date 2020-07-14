cecfacb5b6543dad5f9373f79fc8420c3cafe514
Piotr <mojprogress@gmail.com>
2020-07-14 09:16:49 +0200

    .

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("/url-generator/", views.url_generator, name="url-generator"),
+    path("url-generator/", views.url_generator, name="url-generator"),
     path("<str:shortened_url>/", views.link_redirect, name="link-redirect"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
